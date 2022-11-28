72d8fce6f3b59d07c4033dc6c9f8d2b5bf415db2
KonradS6502 <konradsegelyn@gmail.com>
2022-11-28 18:45:16 +0100

    Update eepromAT24C.h

--- eepromAT24C.h
+++ eepromAT24C.h
@@ -7,7 +7,16 @@
 #define I2C_DATA_RECEIVED                               (TWI_RIF_bm & TWI0.MSTATUS)
 #define I2C_DATA_SEND                               (TWI_WIF_bm & TWI0.MSTATUS)
 
-
+/* 
+ * Initialization of I2C is obligatory before use.
+ * Example of initialization:
+ * 
+ * void I2C_init(void){
+    TWI0.MBAUD=0xFF;
+    TWI0.MCTRLA|=_BV(0);
+    TWI0.MSTATUS|=_BV(0);
+}
+ */
 void I2C_0_transmittingAddrPacket(uint8_t slaveAddres, uint8_t directionBit);
 uint8_t I2C_0_receivingDataPacket(void);
 void I2C_0_sendMasterCommand(uint8_t newCommand);
